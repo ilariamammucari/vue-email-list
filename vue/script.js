a313d0dc0d5e4357407ccbe392f3a63c78430541
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-08 14:19:46 +0100

    Genero 10 mail tramite api e le stampo in una lista

--- vue/script.js
+++ vue/script.js
@@ -1,1 +1,16 @@
-alert()
+// ! Genero 10 mail tramite api e le stampo in una lista
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        mails: []
+    },
+    mounted(){
+        for ( let i = 0; i < 10; i++ ){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                console.log(result);
+                this.mails.push(result.data.response);
+            });
+        }
+    }
+});
